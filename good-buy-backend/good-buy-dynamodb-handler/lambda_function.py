3ca19a4f9cee439e1a79d4592f6aabfffc34e113
Dustin <dcbell3@uno.edu>
2023-03-27 21:05:32 -0500

    Making a change to lambda to make sure it is working as expected

--- good-buy-backend/good-buy-dynamodb-handler/lambda_function.py
+++ good-buy-backend/good-buy-dynamodb-handler/lambda_function.py
@@ -27,7 +27,7 @@ db = session.resource("dynamodb")
 
 def lambda_handler(event, context):
     if event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'GET':
-        table = db.Table("Shopping_Cart")
+        table = db.Table("Shopping_Car")
         print(event['queryStringParameters']['ID'])
         IDvalue = event['queryStringParameters']['ID']
         response = table.get_item(
@@ -42,7 +42,7 @@ def lambda_handler(event, context):
         }
 
     elif event['path'] == '/database/user-account' and event['httpMethod'] == 'GET':
-        table = db.Table("User")
+        table = db.Table("Users")
         print(event['queryStringParameters']['ID'])
         IDvalue = event['queryStringParameters']['ID']
         response = table.get_item(

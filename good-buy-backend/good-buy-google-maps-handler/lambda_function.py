e6a55b460be371db442959a7aa248a96e7eb21b0
johnylopez <johnycrack21@hotmail.com>
2023-04-02 10:33:29 -0500

    updated google maps lambda function

--- good-buy-backend/good-buy-google-maps-handler/lambda_function.py
+++ good-buy-backend/good-buy-google-maps-handler/lambda_function.py
@@ -22,5 +22,10 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
         'body': json.dumps(jsons)
     }

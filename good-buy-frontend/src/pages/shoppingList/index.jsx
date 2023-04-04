8a5b5e18f8050c58d17babb778e84990a44075f5
bigbeanie <120328409+bigbeanie@users.noreply.github.com>
2023-04-04 13:42:22 -0500

    test

--- good-buy-frontend/src/pages/shoppingList/index.jsx
+++ good-buy-frontend/src/pages/shoppingList/index.jsx
@@ -3,7 +3,11 @@ import { motion } from "framer-motion";
 const ShoppingList = () => {
 	return (
 		<motion.div exit={{ opacity: 0 }}>
+			<div className = "List-Container">
+				<h2>Ducks are cool!1111!</h2>
 
+
+			</div>
 		</motion.div>
 	);
 }

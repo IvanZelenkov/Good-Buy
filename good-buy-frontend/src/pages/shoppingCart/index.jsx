c26d910f4a718e9cc734ab354fdb097dce6e6aa5
aa880206tw <59859080+aa880206tw@users.noreply.github.com>
2023-04-04 18:31:22 -0500

    update
    all pick basic top bar

--- good-buy-frontend/src/pages/shoppingCart/index.jsx
+++ good-buy-frontend/src/pages/shoppingCart/index.jsx
@@ -6,6 +6,17 @@ const ShoppingCart = () => {
 			<div className = "Cart-Container">
 			<h1> You set sail, for fail</h1>
 
+				<div className="top">
+					<div className='sel-box'>
+						<input type="checkbox" />
+						<i>全選</i>
+					</div>
+					<span className='imgname-box'>商品名稱</span>
+                <span>單價</span>
+                <span className='count-box'>數量</span>
+                <span>金額</span>
+                <span>操作</span>
+            </div>
 			</div>
 		</motion.div>
 	);

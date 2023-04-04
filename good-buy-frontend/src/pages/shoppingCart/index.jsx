c2fa9d16ae7648baf2a98f91cebde1684a32ceb5
aa880206tw <59859080+aa880206tw@users.noreply.github.com>
2023-04-04 17:39:34 -0500

    Revert "dus"
    This reverts commit eb9e94067e0365cafe5ef3880f1eb0511fbe10fc.

--- good-buy-frontend/src/pages/shoppingCart/index.jsx
+++ good-buy-frontend/src/pages/shoppingCart/index.jsx
@@ -4,7 +4,7 @@ const ShoppingCart = () => {
 	return (
 		<motion.div exit={{ opacity: 0 }}>
 			<div className = "Cart-Container">
-			<h1> You set sail, for fail</h1>
+			<h2> You set sail, for fail</h2>
 
 			</div>
 		</motion.div>

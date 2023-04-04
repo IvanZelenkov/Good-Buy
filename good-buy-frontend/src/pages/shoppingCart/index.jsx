cfd414dc62e2189526e20f259ede5229567f8904
aa880206tw <59859080+aa880206tw@users.noreply.github.com>
2023-04-04 17:40:19 -0500

    Revert "push test"
    This reverts commit 83a7bbee12bef819ab1cc38d3288e9184cd6f148.

--- good-buy-frontend/src/pages/shoppingCart/index.jsx
+++ good-buy-frontend/src/pages/shoppingCart/index.jsx
@@ -3,18 +3,9 @@ import { motion } from "framer-motion";
 const ShoppingCart = () => {
 	return (
 		<motion.div exit={{ opacity: 0 }}>
-			<div className = "Cart-Container">
-			<h2> You set sail, for fail</h2>
 
-			</div>
 		</motion.div>
-		
 	);
 }
-
-
-
-
-
 
 export default ShoppingCart;

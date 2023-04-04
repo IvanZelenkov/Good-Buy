f56ba1836087bb24bfbe98eb9cfa8401aa31b663
aa880206tw <59859080+aa880206tw@users.noreply.github.com>
2023-04-04 17:48:36 -0500

    ha
    finally update

--- good-buy-frontend/src/pages/shoppingCart/index.jsx
+++ good-buy-frontend/src/pages/shoppingCart/index.jsx
@@ -3,7 +3,10 @@ import { motion } from "framer-motion";
 const ShoppingCart = () => {
 	return (
 		<motion.div exit={{ opacity: 0 }}>
+			<div className = "Cart-Container">
+			<h1> You set sail, for fail</h1>
 
+			</div>
 		</motion.div>
 	);
 }

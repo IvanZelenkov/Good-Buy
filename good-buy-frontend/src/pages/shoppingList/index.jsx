27f934360eab2bdd62f6b3bf9a25555c24359009
bigbeanie <120328409+bigbeanie@users.noreply.github.com>
2023-04-04 14:46:27 -0500

    basic layout
    basic grid layout and test checkbox

--- good-buy-frontend/src/pages/shoppingList/index.jsx
+++ good-buy-frontend/src/pages/shoppingList/index.jsx
@@ -1,14 +1,51 @@
 import { motion } from "framer-motion";
+import * as React from 'react';
+// import React, { Component } from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const ShoppingList = () => {
 	return (
-		<motion.div exit={{ opacity: 0 }}>
-			<div className = "List-Container">
-				<h2>Ducks are cool!1111!</h2>
+		<div
+			className = "List-Container"
+			>
+				
+			<h2 style = {{color:"black", textAlign: "center", fontSize: 50}}>ShoppingList</h2>	
 
+			<Grid container spacing={5}
+				justifyContent = "center"
+				display = "flex"
+				alignItems = "stretch"
+				direction = "row">
+  				<Grid item xs={1}>
+				  	<Box style = {{color: "black"}}>itemname</Box>
 
+  				</Grid>
+  				<Grid item xs={4.5}>
+    				<Box style = {{color: "black"}}>itemname</Box>
+  				</Grid>
+  				<Grid item xs ={1}>
+    				<Box style = {{color: "black"}}>checkbox</Box>
+  				</Grid>
+				<Grid item xs ={4.5}>
+					<Box style = {{color: "black"}}>itemname</Box>
+				</Grid>
+			</Grid>
+
+			<div>
+				<Checkbox {...label} defaultChecked />
+				<Checkbox {...label} />
+				<Checkbox {...label} disabled />
+				<Checkbox {...label} disabled checked />
 			</div>
-		</motion.div>
+
+		</div>
+
+
+
 	);
 }
 

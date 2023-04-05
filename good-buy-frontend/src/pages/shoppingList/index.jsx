63c909a159cab2a888104e670c7cb95d7ae80b2e
bigbeanie <120328409+bigbeanie@users.noreply.github.com>
2023-04-05 16:04:34 -0500

    Functions + Multiple Checkboxes
    Functionality is the exact same, no new implementation, just ground work laid for new implementation.

--- good-buy-frontend/src/pages/shoppingList/index.jsx
+++ good-buy-frontend/src/pages/shoppingList/index.jsx
@@ -6,41 +6,59 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const name = "ShoppingList";
+const itemCount = 2;
+const shopCount = 2;
+
+
 
 const ShoppingList = () => {
+
+	const generateGrid = () => {
+		return(
+				<Grid container spacing={5}
+					justifyContent = "center"
+					display = "flex"
+					alignItems = "stretch"
+					direction = "row">
+				  <Grid item xs={1}>
+					  <div>
+						<Checkbox {...label} defaultChecked />
+						<Checkbox {...label} />
+						<Checkbox {...label} disabled />
+						<Checkbox {...label} disabled checked />
+					</div>
+			
+					  </Grid>
+					  <Grid item xs={4.5}>
+						<Box style = {{color: "black"}}>itemname</Box>
+					  </Grid>
+					  <Grid item xs ={1}>
+						  <div>
+							<Checkbox {...label} defaultChecked />
+							<Checkbox {...label} />
+							<Checkbox {...label} disabled />
+							<Checkbox {...label} disabled checked />
+						</div>
+					  </Grid>
+					<Grid item xs ={4.5}>
+						<Box style = {{color: "black"}}>itemname</Box>
+					</Grid>
+				</Grid>
+			
+			);
+
+
+	}
+
 	return (
 		<div
 			className = "List-Container"
 			>
 				
-			<h2 style = {{color:"black", textAlign: "center", fontSize: 50}}>ShoppingList</h2>	
+			<h2 style = {{color:"black", textAlign: "center", fontSize: 50}}>{name}</h2>	
 
-			<Grid container spacing={5}
-				justifyContent = "center"
-				display = "flex"
-				alignItems = "stretch"
-				direction = "row">
-  				<Grid item xs={1}>
-				  	<Box style = {{color: "black"}}>itemname</Box>
-
-  				</Grid>
-  				<Grid item xs={4.5}>
-    				<Box style = {{color: "black"}}>itemname</Box>
-  				</Grid>
-  				<Grid item xs ={1}>
-    				<Box style = {{color: "black"}}>checkbox</Box>
-  				</Grid>
-				<Grid item xs ={4.5}>
-					<Box style = {{color: "black"}}>itemname</Box>
-				</Grid>
-			</Grid>
-
-			<div>
-				<Checkbox {...label} defaultChecked />
-				<Checkbox {...label} />
-				<Checkbox {...label} disabled />
-				<Checkbox {...label} disabled checked />
-			</div>
+			{generateGrid()}
 
 		</div>
 
@@ -48,5 +66,7 @@ const ShoppingList = () => {
 
 	);
 }
+
+
 
 export default ShoppingList;

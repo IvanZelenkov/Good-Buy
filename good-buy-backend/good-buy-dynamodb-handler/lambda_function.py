e295e5f7e9c6e73c618b2ba7c67eb2ec1ab383ce
Dustin <dcbell3@uno.edu>
2023-04-11 22:17:43 -0500

    Updated lambda function to respond to pylint syntax suggestions

--- good-buy-backend/good-buy-dynamodb-handler/lambda_function.py
+++ good-buy-backend/good-buy-dynamodb-handler/lambda_function.py
@@ -1,7 +1,13 @@
+'''
+This module defines the lambda function that will return interact with
+the DynamoDB database. When invoked, it will take an event as input
+and take query string parameters or body data from it.
+'''
+
 import json
-import boto3
 import os
 from abc import ABC, abstractmethod
+import boto3
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -19,46 +25,55 @@ access_key = os.getenv('ACCESS_KEY')
 
 secret_access_key = os.getenv('SECRET_ACCESS_KEY')
 
-session = boto3.Session(aws_access_key_id = access_key, aws_secret_access_key = secret_access_key, region_name = "us-east-1")
+session = boto3.Session(aws_access_key_id = access_key,
+aws_secret_access_key = secret_access_key,
+region_name = "us-east-1")
 
 db = session.resource("dynamodb")
 
 
 
 
-def lambda_handler(event, context):
+def lambda_handler(event):
+    '''Lambda function that will take in an event and perform
+    different actions depending on the path and HTTP Method
+    sent in the event parameter.
+    '''
     print(event)
-    # When using this path and method, must provide ID of the shopping cart you want to retrieve as a query parameter.
+    # When using this path and method, must provide ID of the shopping
+    # cart you want to retrieve as a query parameter.
     if event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'GET':
         table = db.Table("Shopping_Cart")
-        getAction = GetAction(event, table)
-        response = getAction.action()
+        get_action = GetAction(event, table)
+        response = get_action.action()
         # print(response['Item']['cart'])
         print(response['Item'])
         return {
             'statusCode': 200,
             'body': json.dumps(response['Item']['cart'], indent=2,default=str)
         }
-    # When using this path and method, must provide ID of the user account you want to retrieve as a query parameter.
-    elif event['path'] == '/database/user-account' and event['httpMethod'] == 'GET':
+    # When using this path and method, must provide ID of the user account you
+    # want to retrieve as a query parameter.
+    if event['path'] == '/database/user-account' and event['httpMethod'] == 'GET':
         table = db.Table("Users")
-        getAction = GetAction(event, table)
-        response = getAction.action()
+        get_action = GetAction(event, table)
+        response = get_action.action()
         print(response['Item'])
         return {
             'statusCode': 200,
             'body': json.dumps(response['Item'], indent=2,default=str)
         }
-    # When using this path and method, must provide the ID and the cart values the newly created cart as json body data.
-    elif event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'POST':
+    # When using this path and method, must provide the ID and the cart
+    # values the newly created cart as json body data.
+    if event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'POST':
         table = db.Table("Shopping_Cart")
-        decodedEvent = json.loads(event['body'])
-        Item ={
-                'ID': int(decodedEvent['ID']),
-                'cart': decodedEvent['cart']
+        decoded_event = json.loads(event['body'])
+        item ={
+                'ID': int(decoded_event['ID']),
+                'cart': decoded_event['cart']
         }
-        postAction = PostAction(event, table, Item)
-        response = postAction.action()
+        post_action = PostAction(event, table, item)
+        response = post_action.action()
         return {
             'statusCode': 200,
             'headers': {
@@ -68,19 +83,20 @@ def lambda_handler(event, context):
             },
             'body': 'Created new shopping cart'
         }
-    # When using this path and method, must provide the ID email, password, phone, and username values the newly created cart as json body data.
-    elif event['path'] == '/database/user-account' and event['httpMethod'] == 'POST':
+    # When using this path and method, must provide the ID email, password,
+    # phone, and username values the newly created cart as json body data.
+    if event['path'] == '/database/user-account' and event['httpMethod'] == 'POST':
         table = db.Table("Users")
-        decodedEvent = json.loads(event['body'])
-        Item ={
-                'ID': int(decodedEvent['ID']),
-                'email': decodedEvent['email'],
-                'password': decodedEvent['password'],
-                'phone': decodedEvent['phone'],
-                'username': decodedEvent['username']
+        decoded_event = json.loads(event['body'])
+        item ={
+                'ID': int(decoded_event['ID']),
+                'email': decoded_event['email'],
+                'password': decoded_event['password'],
+                'phone': decoded_event['phone'],
+                'username': decoded_event['username']
             }
-        postAction = PostAction(event, table, Item)
-        response = postAction.action()
+        post_action = PostAction(event, table, item)
+        response = post_action.action()
         return {
             'statusCode': 200,
             'headers': {
@@ -92,25 +108,26 @@ def lambda_handler(event, context):
         }
 
     # Idea for updating cart
-    # When an item is added or removed from the cart, it should send body data of the updated cart.
-    # Then retrieve that body data to update it with. 
-    # When using this path and method, send the ID of the shopping cart you want to update as a query parameter and then 
-    # pass the new cart data as body data. 
-    elif event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'PUT':
-        
+    # When an item is added or removed from the cart,
+    # it should send body data of the updated cart.
+    # Then retrieve that body data to update it with.
+    # When using this path and method, send the ID of the
+    # shopping cart you want to update as a query parameter and then
+    # pass the new cart data as body data.
+    if event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'PUT':
         table = db.Table("Shopping_Cart")
-        decodedEvent = json.loads(event['body'])
-        
-        UpdateExpression = "SET cart =:cart"
-        ExpressionAttributeValues = {
-            ':cart' : decodedEvent['cart']
+        decoded_event = json.loads(event['body'])
+
+        update_expression = "SET cart =:cart"
+        expression_attribute_values = {
+            ':cart' : decoded_event['cart']
         }
 
-        # decodedExpressionAttributes = json.dumps(ExpressionAttributeValues)
+        # decodedExpressionAttributes = json.dumps(expression_attribute_values)
 
-        putAction = PutAction(event, table, UpdateExpression, ExpressionAttributeValues)
-        response = putAction.action()
-        
+        put_action = PutAction(event, table, update_expression, expression_attribute_values)
+        response = put_action.action()
+
         return {
             'statusCode': 200,
             'headers': {
@@ -120,22 +137,23 @@ def lambda_handler(event, context):
             },
             'body': 'Updated a shopping cart'
         }
-    # When using this path and method, must pass the ID of the user accound you want to update as a query parameter,
+    # When using this path and method, must pass the ID of
+    # the user accound you want to update as a query parameter,
     # and then pass the new user acount data as body data.
-    elif event['path'] == '/database/user-account' and event['httpMethod'] == 'PUT':
-        
+    if event['path'] == '/database/user-account' and event['httpMethod'] == 'PUT':
+
         table = db.Table("Users")
-        decodedEvent = json.loads(event['body'])
-        
-        UpdateExpression = "SET password = :password, phone = :phone, username = :username"
-        ExpressionAttributeValues = {
-            ':password' : decodedEvent['password'],
-            ':phone' : decodedEvent['phone'],
-            ':username': decodedEvent['username']
+        decoded_event = json.loads(event['body'])
+
+        update_expression = "SET password = :password, phone = :phone, username = :username"
+        expression_attribute_values = {
+            ':password' : decoded_event['password'],
+            ':phone' : decoded_event['phone'],
+            ':username': decoded_event['username']
         }
 
-        putAction = PutAction(event, table, UpdateExpression, ExpressionAttributeValues)
-        response = putAction.action()
+        put_action = PutAction(event, table, update_expression, expression_attribute_values)
+        response = put_action.action()
 
         return {
             'statusCode': 200,
@@ -146,12 +164,13 @@ def lambda_handler(event, context):
             },
             'body': 'Updated a user account'
         }
-    # When using this path and method, must pass the ID of the shopping cart that you want to delete as a query parameter.
-    elif event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'DELETE':
+    # When using this path and method, must pass the ID of the shopping cart
+    # that you want to delete as a query parameter.
+    if event['path'] == '/database/shopping-cart' and event['httpMethod'] == 'DELETE':
         table = db.Table("Shopping_Cart")
-        
-        deleteAction = DeleteAction(event, table)
-        response = deleteAction.action()
+
+        delete_action = DeleteAction(event, table)
+        response = delete_action.action()
 
         return {
             'statusCode': 200,
@@ -162,13 +181,14 @@ def lambda_handler(event, context):
             },
             'body': 'Deleted a shopping cart'
         }
-    # When using this path and method, must pass the ID of the user acount you want to delete at query parameter. 
-    elif event['path'] == '/database/user-account' and event['httpMethod'] == 'DELETE':
+    # When using this path and method, must pass the ID of the user
+    # acount you want to delete at query parameter.
+    if event['path'] == '/database/user-account' and event['httpMethod'] == 'DELETE':
         table = db.Table("Users")
-        
-        deleteAction = DeleteAction(event, table)
-        response = deleteAction.action()
-        
+
+        delete_action = DeleteAction(event, table)
+        response = delete_action.action()
+
         return {
             'statusCode': 200,
             'headers': {
@@ -180,66 +200,94 @@ def lambda_handler(event, context):
         }
 
 class DBActionInterface(ABC):
+    '''
+    Interface for action that will interact with
+    the DynamoDB database.
+    '''
     @abstractmethod
     def action(self):
-        pass
+        '''Perform action'''
 
 class GetAction(DBActionInterface):
-    
+    '''
+    This class defines that will perform a GET action on the 
+    DynamoDB database. This class takes an event parameter and
+    a table parameter. After action is performed it will
+    return response.
+    '''
     def __init__(self, event, table):
         self.event = event
         self.table = table
-    
-    def action(self):        
+
+    def action(self):
         # print(event['queryStringParameters']['ID'])
-        IDvalue = self.event['queryStringParameters']['ID']
+        id_value = self.event['queryStringParameters']['ID']
         response = self.table.get_item(
             Key = {
-                'ID': int(IDvalue)
+                'ID': int(id_value)
             }
         )
         return response
 
 class PostAction(DBActionInterface):
-    def __init__(self, event, table, Item):
+    '''
+    This class defines that will perform a POST action on the 
+    DynamoDB database. This class takes an event parameter and
+    a table parameter and an Item parameter. After action is performed
+    it will return response.
+    '''
+    def __init__(self, event, table, item):
         self.event = event
         self.table = table
-        self.Item = Item
+        self.item = item
 
     def action(self):
         response = self.table.put_item(
-            Item = self.Item
+            Item = self.item
         )
         return response
 
 class PutAction(DBActionInterface):
-    def __init__(self, event, table, updateExpression, expressionAttributeValues):
-        self.event = event 
+    '''
+    This class defines that will perform a PUT action on the 
+    DynamoDB database. This class takes an event parameter and
+    a table parameter an updateExpression parameter, and an
+    expressionAttributeValues parmeter. After action is performed it will
+    return response.
+    '''
+    def __init__(self, event, table, update_expression, expression_attribute_values):
+        self.event = event
         self.table = table
-        self.updateExpression = updateExpression
-        self.expressionAttributeValues = expressionAttributeValues
+        self.update_expression = update_expression
+        self.expression_attribute_values = expression_attribute_values
 
     def action(self):
-        IDvalue = self.event['queryStringParameters']['ID']
+        id_value = self.event['queryStringParameters']['ID']
         # decodedExpressionAttributes = json.loads(self.expressionAttributeValues)
         response = self.table.update_item(
             Key = {
-                'ID' : int(IDvalue)
+                'ID' : int(id_value)
             },
-            UpdateExpression = self.updateExpression,
-            ExpressionAttributeValues = self.expressionAttributeValues
+            UpdateExpression = self.update_expression,
+            ExpressionAttributeValues = self.expression_attribute_values
         )
         return response
 
 class DeleteAction(DBActionInterface):
+    '''
+    This class defines that will perform a DELETE action on the 
+    DynamoDB database. This class takes an event parameter and
+    a table parameter. After action is performed it will
+    return response.
+    '''
     def __init__(self, event, table):
         self.event = event
-        self.table = table 
+        self.table = table
     def action(self):
-        IDvalue = self.event['queryStringParameters']['ID']
+        id_value = self.event['queryStringParameters']['ID']
         response = self.table.delete_item(
             Key = {
-                'ID' : int(IDvalue)
+                'ID' : int(id_value)
             }
         )
         return response

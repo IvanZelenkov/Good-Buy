2e029835f4316079b245cfe69a9126118cce35bc
IvanZelenkov <itproger.ivan@gmail.com>
2023-04-13 12:35:18 -0500

    Fixed pylint errors of good-buy-store-apis-handler.

--- good-buy-backend/good-buy-store-apis-handler/src/service/StoreApiInterface.py
+++ good-buy-backend/good-buy-store-apis-handler/src/service/StoreApiInterface.py
@@ -1,7 +1,13 @@
+"""
+StoreApiInterface module: provides an abstract interface for interacting with store APIs.
+"""
+
 from abc import ABCMeta, abstractmethod
 
 
 class StoreApiInterface(metaclass=ABCMeta):
+    """An abstract interface for interacting with store APIs."""
+
     @abstractmethod
     def get_all_products(self) -> dict:
         """Return all products from the store."""
--- good-buy-backend/good-buy-store-apis-handler/src/strategy/StoreNameStrategy.py
+++ good-buy-backend/good-buy-store-apis-handler/src/strategy/StoreNameStrategy.py
@@ -1,20 +1,27 @@
+"""
+This module provides an implementation of a filter strategy to filter products by store name.
+"""
+
 import json
-from FilterStrategyInterface import FilterStrategyInterface
 from typing import Dict, Any
+from FilterStrategyInterface import FilterStrategyInterface
 from S3Service import S3Service
 
 
 class StoreNameStrategy(FilterStrategyInterface):
+    """
+    This class implements the FilterStrategyInterface to filter products by store name.
+    """
     def filter(self, s3_service: S3Service, params: dict) -> Dict[str, Any]:
         """
-        Retrieves a JSON object from an S3 bucket based on the given event dictionary.
+        Filters products by store name.
 
         Args:
-            s3_service: An instance of the S3Service class used to connect to S3.
+            s3_service: An instance of the S3Service class is used to connect to the S3 service.
             params: A dictionary containing parameter "store_name": "rouses|walmart|winn_dixie"
 
         Returns:
-            A dictionary containing the parsed JSON data.
+            A list of products that match the store name criteria.
         """
         try:
             return json.loads(s3_service.get_s3_object(params.get("store_name")))

8e3dfa4c61e6d899f952e594b6138aee0b46d68c
aa880206tw <59859080+aa880206tw@users.noreply.github.com>
2023-04-13 20:24:31 -0500

    add ui to shoppingcart
    thx Ivan

--- good-buy-frontend/src/pages/shoppingCart/ProductCard.js
+++ good-buy-frontend/src/pages/shoppingCart/ProductCard.js
@@ -1,13 +1,36 @@
 import { Card, Button, Form, Col,Row } from "react-bootstrap";
+import  {CartContext}  from "./CartContext"
+import {useContext} from 'react';
+
+
+
 
 function ProductCard(props){
     const product =props.product;
+    const cart =useContext(CartContext);
+    const productQuanity = cart.getProductQuanity(product.id);
+    console.log (cart.item);
+
     return(
         <Card>
             <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
-                <Button variant="primary">Add to Cart</Button>
+                {productQuanity > 0 ?
+                <>
+                    <Form as ={Row}>
+                        <Form.Label column ="true" sm ="6">In cart: {productQuanity}</Form.Label>
+                        <Col sm="6">
+                            <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className="mx-2">+</Button>
+                            <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)}className="mx-2">-</Button>
+                        </Col>
+                    </Form>
+                    <Button variant="danger" onClick={() => cart.removeEverthingFromCart(product.id)} className="my-2">REMOVE FROM CART</Button>
+                </>
+                :
+                <Button variant="primary" onClick={() => cart.addOneToCart(product.id)}>Add to Cart</Button>
+                }
+                
             </Card.Body>
         </Card>
     )
--- good-buy-frontend/src/pages/shoppingCart/index.jsx
+++ good-buy-frontend/src/pages/shoppingCart/index.jsx
@@ -1,164 +1,155 @@
-import { useState } from 'react';
+import { useState, useMemo, useContext } from "react";
+import {
+	Box,
+	Button,
+	Checkbox,
+	IconButton,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableFooter,
+	TableHead,
+	TableRow,
+	TextField
+} from "@mui/material";
 import { motion } from "framer-motion";
-import { tokens } from '../../theme';
-import * as React from 'react';
-import defImg  from '../../images/product_sample.png';
-import {Box, Container, useTheme} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import { useTheme } from "@mui/material/styles";
+import { tokens } from "../../theme";
+import {ProductCard} from "./CartProduct";
+import { CartContext } from "./CartContext";
+import { getProductData } from "./productStore";
+import { productArray } from "./productStore";
 
-const name = "ShoppingCart";
+const ShoppingCart = (props) => {
+	const { palette: { mode } } = useTheme();
+	const colors = useMemo(() => tokens(mode), [mode]);
+	const topBarHeight = 65;
+	const cart = useContext(CartContext);
+	const id = props.id;
+	const quantity = props.quantity;
+	const productData = getProductData(id);
+	
+	const [items, setItems] = useState([
+		{ id: 1, name: "Three Musketeers" },
+		{ id: 2, name: "Candy Cane" },
+		{ id: 3, name: "Kit Kat" },
+		{ id: 4, name: "Skittles" },
+		{ id: 5, name: "Twix" }
+	]);
+	const [newItem, setNewItem] = useState("");
+    
+	const handleCheck = (id) => {
+		const updatedItems = items.map((item) =>
+			item.id === id ? { ...item, checked: !item.checked } : item
+		);
+		setItems(updatedItems);
+	};
 
-const ShoppingCart = () => {
-	const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+	const handleRemove = (id) => {
+		const updatedItems = items.filter((item) => item.id !== id);
+		setItems(updatedItems);
+	};
+
+	const handleAdd = (event) => {
+		event.preventDefault();
+		const newId = items.length + 1;
+		const newItemObj = { id: newId, name: newItem };
+		setItems([...items, newItemObj]);
+		setNewItem("");
+	};
     const [inputProductName, setInputProductName] = useState("");
 	const [isValid, setIsValid] = useState(false);
 
+
 	return (
-        <Container className="my-3">
-		    <div className="top">
-                <div className='sel-box'>
-                    <input type="checkbox" />
-                    <i>pick all</i>
-                </div>
-                <span className='imgname-box'>product name</span>
-                <span>price</span>
-                <span className='count-box'>count</span>
-                <span>cost</span>
-                <span>operate</span>
-            </div>
-            <div className="middle">
-                    <ul>
+		<Box
+			component={motion.div}
+			exit={{ opacity: 0 }}
+			sx={{
+				height: `calc(100vh - ${topBarHeight}px - 3vh)`,
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+                flexDirection: "column"
+			}}
+		>
+			<TableContainer
+				component={Paper}
+				sx={{
+					maxWidth: "35vw",
+					borderRadius: "10px",
+					overflow: "hidden",
+					boxShadow: "0px 70px 60px rgba(0, 0, 0, 0.5)",
+					backgroundColor: colors.customColors[1],
+					maxHeight: "60vh",
+					overflowY: "auto",
+					marginBottom: "5vh"
+				}}
+			>
+				<Table aria-label="Shopping List">
+					<TableHead sx={{ backgroundColor: colors.customColors[7], position: "sticky", top: 0, zIndex: "1" }}>
+						<TableRow>
+							<TableCell sx={{ color: colors.customColors[1], fontSize: "1.2vh", fontFamily: "Montserrat" }}>Item</TableCell>
+							<TableCell sx={{ color: colors.customColors[1], fontSize: "1.2vh", fontFamily: "Montserrat" }} align="center">Complete</TableCell>
+							<TableCell sx={{ color: colors.customColors[1], fontSize: "1.2vh", fontFamily: "Montserrat" }} align="center">Remove</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody sx={{ height: "calc(60vh - 2.2rem)", overflowY: "auto", zIndex: "0" }}>
+						{items.map((item) => (
+							<TableRow key={item.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+								<TableCell sx={{ fontSize: "1.2vh", fontFamily: "Montserrat" }}>{item.name}</TableCell>
+								<TableCell align="center">
+									<Checkbox checked={item.checked} onChange={() => handleCheck(item.id)} />
+								</TableCell>
+								<TableCell align="center">
+									<IconButton onClick={() => handleRemove(item.id)}>
+										<DeleteIcon sx={{ color: "#FF2323" }} />
+									</IconButton>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+					<TableFooter>
+						<TableRow>
+							<TableCell colSpan={3} sx={{ position: "sticky", bottom: 0, backgroundColor: colors.customColors[1] }}>
+								<form onSubmit={handleAdd} style={{ display: "flex" }}>
+									<TextField
+										value={newItem}
+										onChange={(event) => setNewItem(event.target.value)}
+										placeholder="Add a new product"
+										sx={{ width: "100%" }}
+									/>
+									<Button variant="contained" type="submit" sx={{ marginLeft: 2, color: colors.customColors[1] }}>
+										<AddIcon sx={{ fontSize: "2vh", color: colors.customColors[1] }}/>
+									</Button>
+								</form>
+							</TableCell>
+						</TableRow>
+					</TableFooter>
+				</Table>
+			</TableContainer>
+            
+            
+            <Box sx={{
+                display: "flex",
+                height: "10vh",
+                width: "50vh",
+				alignItems: "center",
+				justifyContent : "center",
+                backgroundColor: colors.customColors[1],
+				borderRadius:"10px"
+            }}>
+            <h2>totall box</h2>
 
-                        <li className='item'>
+            </Box>
+           
+		</Box>
 
-                            <div className='sel-box'>
-                                <input type="checkbox" />
-                            </div>
-
-                            <div className='imgname-box'>
-                                <img src={defImg} alt="item" />
-                                <span>product nameproduct nameproduct nameproduct nameproduct nameproduct name</span>
-                            </div>
-
-                            <div className='price-box'>
-                                $<span className='price'>0.00</span>
-                            </div>
-
-                            <div className='count-box'>
-                                <button>-</button>
-                                <input type="number" />
-                                <button>+</button>
-                            </div>
-
-                            <div className='amount-box'>
-                                <span className='price'>0.00</span>
-                            </div>
-                            <div className='action-box'>
-                                <a href="#">remove item</a>
-                            </div>
-                        </li>
-                        <li className='item'>
-
-                            <div className='sel-box'>
-                                <input type="checkbox" />
-                            </div>
-
-                            <div className='imgname-box'>
-                                <img src={defImg} alt="item" />
-                                <span>product nameproduct nameproduct nameproduct nameproduct nameproduct name</span>
-                            </div>
-
-                            <div className='price-box'>
-                                $<span className='price'>0.00</span>
-                            </div>
-
-                            <div className='count-box'>
-                                <button>-</button>
-                                <input type="number" />
-                                <button>+</button>
-                            </div>
-
-                            <div className='amount-box'>
-                                <span className='price'>0.00</span>
-                            </div>
-                            <div className='action-box'>
-                                <a href="#">remove item</a>
-                            </div>
-                        </li>
-                        <li className='item'>
-
-                            <div className='sel-box'>
-                                <input type="checkbox" />
-                            </div>
-
-                            <div className='imgname-box'>
-                                <img src={defImg} alt="item" />
-                                <span>product nameproduct nameproduct nameproduct nameproduct nameproduct name</span>
-                            </div>
-
-                            <div className='price-box'>
-                                $<span className='price'>0.00</span>
-                            </div>
-
-                            <div className='count-box'>
-                                <button>-</button>
-                                <input type="number" />
-                                <button>+</button>
-                            </div>
-
-                            <div className='amount-box'>
-                                <span className='price'>0.00</span>
-                            </div>
-                            <div className='action-box'>
-                                <a href="#">remove item</a>
-                            </div>
-                        </li>
-                        <li className='item'>
-
-                            <div className='sel-box'>
-                                <input type="checkbox" />
-                            </div>
-
-                            <div className='imgname-box'>
-                                <img src={defImg} alt="item" />
-                                <span>product nameproduct nameproduct nameproduct nameproduct nameproduct name</span>
-                            </div>
-
-                            <div className='price-box'>
-                                $<span className='price'>0.00</span>
-                            </div>
-
-                            <div className='count-box'>
-                                <button>-</button>
-                                <input type="number" />
-                                <button>+</button>
-                            </div>
-
-                            <div className='amount-box'>
-                                <span className='price'>0.00</span>
-                            </div>
-                            <div className='action-box'>
-                                <a href="#">remove item</a>
-                            </div>
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div className="bottom">
-                    <div className="left"></div>
-                    <div className='count-box'>
-                        Already pick <span className='price'>0</span> items
-                    </div>
-                    <div className='amount-box'>
-                        Totall <span className='price'>0.00</span>
-                    </div>
-                    <div className="pay-box">
-                        <i>Totall</i>
-                    </div>
-                </div>
-        </Container>
 	);
-}
+};
 
 export default ShoppingCart;

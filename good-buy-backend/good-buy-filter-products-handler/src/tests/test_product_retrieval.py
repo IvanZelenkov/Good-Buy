2d3ae3fcdda8f020bd3de833ff681297e2dbe5f0
IvanZelenkov <itproger.ivan@gmail.com>
2023-04-18 16:23:49 -0500

    Added __init__.py

--- good-buy-backend/good-buy-filter-products-handler/src/tests/test_product_retrieval.py
+++ good-buy-backend/good-buy-filter-products-handler/src/tests/test_product_retrieval.py
@@ -1,14 +1,10 @@
 """
-test_product_retrieval.py validates the various attribute values of
-a specific product retrieved from the Store API.
+TODO
 """
-
-# pylint: disable=import-error
-# from lambda_function import lambda_handler
 
 
 def test_lambda_function():
     """
-    Tests if the ID of the first product from Store API equals 84989861.
+    TODO
     """
     return "..."

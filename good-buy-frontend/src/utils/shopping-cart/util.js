c8d5e01b5631b21321707849ad2e07cd5cedfa5d
IvanZelenkov <itproger.ivan@gmail.com>
2023-04-28 21:06:12 -0500

    Committing existing modifications related to the shopping cart page and its functionality.

--- good-buy-frontend/src/utils/shopping-cart/util.js
+++ good-buy-frontend/src/utils/shopping-cart/util.js
@@ -1,114 +1,60 @@
-
 import axios from "axios";
 
-export const handleChange = (event, value, setPage) => {
-	setPage(value);
-};
+export const getShoppingCartData = async (user, state, setState) => {
+	console.log(user)
 
-export const handleFilter = (filter, filters, setFilters) => {
-	const existingFilterIndex = filters.findIndex((f) => f.key === filter.key);
+	if (user.attributes.email) {
+		try {
+			const shoppingCartDataResponse = await axios.get(
+				"https://" +
+				process.env.REACT_APP_REST_API_ID +
+				`.execute-api.us-east-1.amazonaws.com/Development/database/shopping-cart?ID=${user.attributes.email}`
+			);
 
-	if (existingFilterIndex !== -1) {
-		// If a filter with the same key already exists, remove it from the list or update its values
-		const existingFilter = filters[existingFilterIndex];
-		const updatedValues = filter.values || [filter.value];
-		const newValues = existingFilter.value.includes(filter.value)
-			? existingFilter.value.filter((v) => v !== filter.value)
-			: [...existingFilter.value, ...updatedValues];
-		if (newValues.length) {
-			const updatedFilter = {
-				...existingFilter,
-				value: newValues,
-			};
-			const updatedFilters = [...filters];
-			updatedFilters[existingFilterIndex] = updatedFilter;
-			setFilters(updatedFilters);
-		} else {
-			const updatedFilters = filters.filter((f, index) => index !== existingFilterIndex);
-			setFilters(updatedFilters);
+			if (shoppingCartDataResponse.data.Item && shoppingCartDataResponse.data.Item.cart)
+				setState(prevState => ({
+					...prevState,
+					shoppingCartData: shoppingCartDataResponse.data.Item.cart,
+					infoLoaded: true
+				}));
+			else
+				await generateShoppingCartForNewUser(user, state, setState);
+
+		} catch (error) {
+			console.log(error);
 		}
-	} else {
-		// If the filter doesn't exist yet, add it to the list with all its values
-		const allValues = filter.values || [filter.value];
-		const newFilter = {
-			key: filter.key,
-			value: allValues,
-		};
-		setFilters((prevState) => [...prevState, newFilter]);
 	}
 };
 
-export const filterProducts = async (filters, setProductsData, setInfoLoaded) => {
-	const filterPairs = {};
-	filters.forEach((filter) => {
-		if (Array.isArray(filter.value)) {
-			console.log(filter.key)
-			// If the value is an array, join the elements with a comma
-			filterPairs[filter.key] = filter.value.join(",");
-		} else {
-			filterPairs[filter.key] = filter.value;
-		}
-	});
+export const generateShoppingCartForNewUser = async (user, state, setState) => {
+	console.log(user.attributes.email)
 
 	try {
-		const productsDataResponse = await axios.get(
-			"https://" +
-			process.env.REACT_APP_REST_API_ID +
-			".execute-api.us-east-1.amazonaws.com/Development/store-apis/filter-products",
-			{ params: filterPairs }
-		);
-		setProductsData(productsDataResponse.data);
-		setInfoLoaded(true);
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-export const getProductsInPriceRange = async (filter, setProductsData, setInfoLoaded) => {
-	const filterPair = {};
-	filterPair[filter.key] = filter.value;
-
-	try {
-		const productsDataResponse = await axios.get(
+		const shoppingCartDataResponse = await axios.post(
 			"https://" +
 			process.env.REACT_APP_REST_API_ID +
 			".execute-api.us-east-1.amazonaws.com/Development/database/shopping-cart",
-			{ params: filterPair }
+			{
+				"email": user.attributes.email
+			}
 		);
-		setProductsData(productsDataResponse.data);
-		setInfoLoaded(true);
-	} catch (error) {
+
+		console.log("GENERATE SHOPPING CART DATA: ")
+		console.log(shoppingCartDataResponse)
+
+		if (shoppingCartDataResponse.data.Item.cart)
+			setState(prevState => ({
+				...prevState,
+				shoppingCartData: shoppingCartDataResponse.data,
+				infoLoaded: true
+			}));
+	}
+	catch (error) {
 		console.log(error);
 	}
 };
 
-export const sortProducts = async (sorters, setProductsData, setInfoLoaded) => {
-	const sorterPairs = {};
-	sorters.forEach((sorter) => {
-		sorterPairs[sorter.key] = sorter.value;
-	});
-
-	try {
-		const productsDataResponse = await axios.get(
-			"https://" +
-			process.env.REACT_APP_REST_API_ID +
-			".execute-api.us-east-1.amazonaws.com/Development/store-apis/filter-products",
-			{ params: sorterPairs }
-		);
-		console.log(productsDataResponse)
-		setProductsData(productsDataResponse.data);
-		setInfoLoaded(true);
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-export const handleClick = (event, setAnchorEl) => {
-	setAnchorEl(event.currentTarget);
-};
-
-export const handleClose = (event, setAnchorEl) => {
-	event.preventDefault();
-	event.stopPropagation();
-	setAnchorEl(null);
+export const handleRemove = (id, items, setItems) => {
+	const updatedItems = items.filter((item) => item.id !== id);
+	setItems(updatedItems);
 };
